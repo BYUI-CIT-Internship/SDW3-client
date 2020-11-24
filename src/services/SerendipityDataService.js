e39249bad70ada7c83aec893f07029775bb86c93
Davisa94 <Austin.i.davis94@gmail.com>
2020-11-24 13:15:48 -0700

    Wrote first function for Serendipity Data service

--- src/services/SerendipityDataService.js
+++ src/services/SerendipityDataService.js
@@ -1,1 +1,13 @@
 import http from "../http-common";
+
+class SerendipityDataService
+{
+    //In here goes every function needed by the front end and must tie to the
+    //backend
+    getColumnAutocompletes(columnNames)
+    {
+        console.log("Fetching auto completes for the following columns: ");
+        console.log(JSON.stringify(columnNames));
+        return http.get(`/serendipity?columns=${columnNames}`);
+    }
+}
